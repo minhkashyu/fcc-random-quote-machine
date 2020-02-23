6b7a157e751ebc86d0bf1ae75fceb20d201da109
Minh Ta <minh.ta@creativeintersection.com>
2020-02-23 19:00:44 +1000

    downgrade bootstrap to version 3.7.1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.css';
-// eslint-disable-next-line
-import $ from 'jquery';
-// eslint-disable-next-line
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './sass/index.scss';
